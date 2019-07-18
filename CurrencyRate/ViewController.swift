b5bf0ffdbefe40bb4d1c27a7bb07fac2c284f2ed
njall1 <3296510810Git>
2019-07-18 23:33:14 +0300

    Initial

---
+++ CurrencyRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyRate
+//
+//  Created by Vitaliy Rusinov on 18/07/2019.
+//  Copyright © 2019 Vitaliy Rusinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
